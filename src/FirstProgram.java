5757a5a9cb9d02ca5de20ba83620649d7e71cdd7
AmanRegeti <aman.regeti@gmail.com>
2024-10-25 17:24:08 -0700

    Added some more lines

--- src/FirstProgram.java
+++ src/FirstProgram.java
@@ -3,8 +3,10 @@ public class FirstProgram {
     // main method is an entry point of your program
     // it should be written exactly in the same format
     public static void main(String[] args) {
-        // short cut - sout
-        System.out.println("Jai Guru Dev");
+        // shortcut - sout
+        System.out.println("Hello World");
+        System.out.println("My Name is Aman");
+        System.out.println("My first Java program");
     }
 
 }
