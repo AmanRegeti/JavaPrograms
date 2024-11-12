a6a2fb9fb03fc1b51969c2c8b2517fbbe9cbbe16
AmanRegeti <aman.regeti@gmail.com>
2024-11-11 16:50:42 -0800

    Added some more lines

--- src/OperatorsPractice.java
+++ src/OperatorsPractice.java
@@ -16,13 +16,70 @@ public class OperatorsPractice {
         System.out.println("i1:"+ i1);
 
         // ++, --
-        int i2 = a++;
+        /*
+            a++ and ++a are the same
+            b=a++ would make b=a and a=a+1
+            b=++a would make a=a+1 b=a
+         */
         int i3 = ++a;
+        int i2 = a++;
         System.out.println("i2:" +i2);
         System.out.println("i3: " +i3);
+
+        int i4 = --a;
+        System.out.println("i4: " + i4);
+        System.out.println("a: "+a);
+
+        int i5 = a--;
+        System.out.println("i5: " + i5);
+        System.out.println("a: "+a);
+
+        int c= ++a;
+        System.out.println("c: " + c);
+        System.out.println("a: "+a);
+
+        int d = a++;
+        System.out.println("d: " + d);
+        System.out.println("a: "+a);
+
         // +=, -=, *=, /=
+        /*
+        v += 4 is same as v = v+4
+        v -= 4 is same as v = v-4
+        v *= 4 is same as v = v*4
+        v /= 4 is same as v = v/4
+         */
+
+        int e = 32;
+        e+=4;
+        System.out.println("e: "+e);
+
+        e-=26;
+        System.out.println("e :" +e);
+
+        e*=53;
+        System.out.println("e :" +e);
+
+        e/=16;
+        System.out.println("e :" +e);
 
         // >, <, >=, <=
+        /*
+        d>33 means is d greater than 33
+        d<33 means is d less than 33
+        d>=33 means is d greater than or equal to 33
+        d<=33 means is d less than or equal to 33
+         */
+        byte f = 31;
+        boolean g = f> 24;
+        System.out.println("g:" + g);
+        boolean b1 = f<232;
+        System.out.println("b1:" + b1);
+        boolean b2 = f<=31;
+        System.out.println("b2:" + b2);
+        boolean b3 = f>= 63;
+        System.out.println("b3:" + b3);
+
 
         // &&, ||, !, ==, !=
         // and, or, not, equals, not equals
---
+++ src/practiceoperators.java
@@ -0,0 +1,8 @@
+public class practiceoperators {
+    public static void main(String[] args) {
+
+    }
+    int c1 = 143%1 ;
+
+
+}
