41706d01c41920e27d14313761634d8a883ace4e
AmanRegeti <aman.regeti@gmail.com>
2024-11-23 16:15:29 -0800

    finding min and max elements of an array

---
+++ src/minandmax.java
@@ -0,0 +1,25 @@
+public class minandmax {
+    public static void main(String[] args) {
+        int[] a = {89, 34, 23, 67, 45, 33, 21, 75, 42, 91};
+        int hv =a[0];
+        int lv =a[0];
+
+        for (int i=1; i<a.length; i++  ){
+            if (a[i]> hv){
+                hv= a[i];
+
+            }
+
+        }
+        System.out.println("The highest value is " + hv);
+        System.out.println("and");
+
+        for (int j=1; j<a.length; j++){
+            if (a[j]< lv){
+                lv= a[j];
+
+            }
+        }
+        System.out.println("The lowest value is " + lv);
+    }
+}
