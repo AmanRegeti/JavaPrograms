cf5608831d9ec487d05d54beef7b75d04cf19124
AmanRegeti <aman.regeti@gmail.com>
2024-11-24 16:22:50 -0800

    finding duplicate with no variables in an array

--- src/findingduplicatesnovariables.java
+++ src/findingduplicatesnovariables.java
@@ -1,19 +1,29 @@
 public class findingduplicatesnovariables {
-    public static void main(String[] args) {
+    public static void main(String[] args)
+    {
+        boolean dupFound = false;
         int[] a = {1, 2, 2, 3, 4, 5, 3, 4, 1};
-        for (int i = 0; i < a.length ; i++) {
-            for (int j = i+1; j < a.length ; j++) {
-                //for (int j = i++; j < a.length ; j++) {
-                    if (a[i] == a[j]) {
-                        System.out.println("Yes, there are duplicates");
+        for (int i = 0; i < a.length ; i++)
+        {
+            for (int j = i+1; j < a.length ; j++)
+            {
+                if (a[i] == a[j])
+                {
+                    dupFound = true;
+
                         break;
                     }
 
                 }
+            if(dupFound)
+            {
+                System.out.println("Yes, there are duplicates");
+                break;
             }
-//            else {
-//                System.out.println("no there are not duplicates ");
-//            }
+
+
+            }
+
         }
     }
 
