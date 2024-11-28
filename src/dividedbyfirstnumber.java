6d3b53165b9cdc5ff414090f8a9a352a5a68d562
AmanRegeti <aman.regeti@gmail.com>
2024-11-28 12:55:36 -0800

    finding what number can be divided by first even number and checking if a number is prime or not.

--- src/dividedbyfirstnumber.java
+++ src/dividedbyfirstnumber.java
@@ -1,20 +1,36 @@
 public class dividedbyfirstnumber {
     public static void main(String[] args) {
-        int[] a={1,3,6,13,12,24,15,16,13};
+        int[] a = {1, 3, 6, 13, 12, 24, 15, 16, 13};
 
-        for (int i = 0; i<a.length; i++){
-            if (a[i]% 2==0){
+        //Method 1;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
                 System.out.println(a[i]);
-                for (int j=i+1; j<a.length; j++){
-                    boolean canbedivded = a[j] % a[i]==0;
-                    if (canbedivded){
-                        System.out.println(a[j] + "can be divided by" + a[i]);
-
-
+                for (int j = i + 1; j < a.length; j++) {
+                    boolean canbedivded = a[j] % a[i] == 0;
+                    if (canbedivded) {
+                        System.out.println(a[j] + " can be divided by " + a[i]);
                     }
-
                 }
                 break;
+            }
+
+        }
+        System.out.println("---------------------------------------------------------------------------------------");
+
+        //Method 2;
+        //int[] a = {1, 3,6, 13, 12, 24, 15, 16, 13};
+
+        int i;
+        for (i=0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                System.out.println(a[i]);
+                break;
+            }
+        }
+        for (int j = i + 1; j < a.length; j++) {
+            if (a[j] % a[i] == 0) {
+                System.out.println(a[j] + " can be divided by " + a[i]);
             }
 
         }
