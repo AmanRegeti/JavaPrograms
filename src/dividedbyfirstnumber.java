b6170f91f65ad4e08db2dda50f58c65eb3212999
AmanRegeti <aman.regeti@gmail.com>
2024-12-02 16:48:34 -0800

    other ways to write the answer for homework

--- src/dividedbyfirstnumber.java
+++ src/dividedbyfirstnumber.java
@@ -20,19 +20,72 @@ public class dividedbyfirstnumber {
 
         //Method 2;
         //int[] a = {1, 3,6, 13, 12, 24, 15, 16, 13};
+        {
+            int i;
+            for (i = 0; i < a.length; i++) {
+                if (a[i] % 2 == 0) {
+                    //    System.out.println(a[i]);
 
-        int i;
-        for (i=0; i < a.length; i++) {
-            if (a[i] % 2 == 0) {
-                System.out.println(a[i]);
-                break;
+                    break;
+                }
+            }
+            int element = a[i];
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] % element == 0) {
+                    System.out.println(a[j] + " can be divided by " + element);
+                }
+
             }
         }
-        for (int j = i + 1; j < a.length; j++) {
-            if (a[j] % a[i] == 0) {
-                System.out.println(a[j] + " can be divided by " + a[i]);
+        System.out.println("-----------------------------------");
+        {
+
+            int i;
+            int element=0;
+            for (i=0; i < a.length; i++) {
+                if (a[i] % 2 == 0) {
+                    //    System.out.println(a[i]);
+                    element = a[i];
+                    break;
+                }
             }
 
+            for (int j = i + 1; j < a.length; j++) {
+                if (a[j] % element == 0) {
+                    System.out.println(a[j] + " can be divided by " + element);
+                }
+
+            }
+
+
+
         }
+
+        System.out.println("-----------------------------------");
+        {
+
+            for (int i=0; i < a.length; i++) {
+                int element = a[i];
+                if (element % 2 == 0) {
+                    //    System.out.println(element);
+
+                    for (int j = i + 1; j < a.length; j++) {
+                        if (a[j] % element == 0) {
+                            System.out.println(a[j] + " can be divided by " + element);
+                        }
+
+                    }
+                    break;
+                }
+
+            }
+
+
+
+
+
+        }
+
+
     }
 }
