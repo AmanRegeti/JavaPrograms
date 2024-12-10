bdb7a43a52d47f4f6bc0eb562d669438f3b6e2d6
AmanRegeti <aman.regeti@gmail.com>
2024-12-09 21:42:28 -0800

    figure out if the next number in array is greater.

--- src/FindingMultiples.java
+++ src/FindingMultiples.java
@@ -1,27 +1,42 @@
+import java.util.Arrays;
+
 public class FindingMultiples {
     public static void main(String[] args) {
         int[] a = {1, 3, 12, 13, 6, 24, 15, 16, 13};
-        int elements = 0;
+        int maxresultsize = (a.length * a.length) - a.length;
+        int[] result = new int[maxresultsize];
+        int counter = -1;
+
         for (int i = 0; i < a.length; i++) {
             int element = a[i];
 
-            System.out.println("--------------------");
-            System.out.println("checking for " + element);
             for (int j = 0; j < a.length; j++) {
 
                 if (j != i) {
-
                     if (a[j] % element == 0) {
                         //System.out.println("     " + a[j] + " can be divided by " + element);
-                        elements++;
-                        int element1 = a[j];
+                        counter++;
+                        int matchedElement = a[j];
                         //int[] result = new int [10];
-                        result[elements] = element1;
-                        System.out.println(result);
+                        result[counter] = matchedElement;
                     }
 
                 }
+
+
             }
         }
+        /*if (counter >= 0) {
+            //result = new int [counter];
+            System.out.println(Arrays.toString(result));
+        } else {
+            System.out.println("no multiples");
+        }*/
+        for (int k=0;k<counter-1;k++){
+            System.out.print(result[k]+",");
+        }
+        System.out.println();
     }
 }
+
+
---
+++ src/GreaterThanNext.java
@@ -0,0 +1,14 @@
+public class GreaterThanNext {
+    public static void main(String[] args) {
+        int[] a = {11, 21, 31, 5, 7, 9};
+        for (int i = 0; i < a.length-1; i++) {
+
+                if(a[i+1]<a[i]) {
+
+                    System.out.println(a[i+1]);
+                    System.out.println(a[i] - a[i+1]);
+                }
+
+        }
+    }
+}
