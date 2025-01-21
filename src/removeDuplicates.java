c68ed9b62ddb0752a297fff7f1cd3d3d348267aa
AmanRegeti <aman.regeti@gmail.com>
2025-01-20 16:47:36 -0800

    moving all the 0s to the right and removing duplicates

---
+++ src/removeDuplicates.java
@@ -0,0 +1,9 @@
+public class removeDuplicates {
+    public static void main(String[] args) {
+        int[] a = {11,11,11,12, 13,13,14,15};
+    }
+
+    private static void removeDuplicates(int[] a) {
+        for(int i = 0)
+    }
+}
