2df4cdad0e176d1ec61f62cb8c57e78b6480cb91
AmanRegeti <aman.regeti@gmail.com>
2025-02-12 09:27:11 -0800

    Added a comment to check git CLI

--- src/StringEndsWith.java
+++ src/StringEndsWith.java
@@ -4,6 +4,8 @@ public class StringEndsWith {
         String suffix= "Regeti";
         name.toCharArray();
         suffix.toCharArray();
+
+        //Added a new line to test git command line.
     }
     private static void FindIfSuffix(char[] name, char[] suffix){
 
