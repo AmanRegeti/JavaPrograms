59ad4c8c30533e91c01add8458301bede9ecad5e
AmanRegeti <aman.regeti@gmail.com>
2025-02-12 09:40:07 -0800

    Removed a comment

--- src/StringEndsWith.java
+++ src/StringEndsWith.java
@@ -5,7 +5,6 @@ public class StringEndsWith {
         name.toCharArray();
         suffix.toCharArray();
 
-        //Added a new line to test git command line.
     }
     private static void FindIfSuffix(char[] name, char[] suffix){
 
