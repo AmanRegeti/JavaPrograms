bf66315e4d794f8bae50ebd81d8c09ddf8aaa678
AmanRegeti <aman.regeti@gmail.com>
2025-02-13 13:00:43 -0800

    test

--- src/ArrayLoop.java
+++ src/ArrayLoop.java
@@ -1,7 +1,7 @@
 public class ArrayLoop {
     public static void main(String[] args) {
         int[] a = {11,34,23,67,45,33,21};
-
+        //Added a comment
         for(int i =0; i < a.length; i++) {
             int element = a[i];
             System.out.println(element);
