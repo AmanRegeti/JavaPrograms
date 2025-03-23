6b3ec86f72306043f7a007be9bbfd05a3b56872b
AmanRegeti <aman.regeti@gmail.com>
2025-03-23 16:36:33 -0700

    take two consecutive numbers in an array, add them and find a number smaller than the sum and add them. And Pattern Practice

--- src/BiggerThanSumOfTwo.java
+++ src/BiggerThanSumOfTwo.java
@@ -1,27 +1,34 @@
-public class BiggerThanSumOfFirstTwo {
+// take two consecutive numbers in an array, add them and find a number smaller than the sum and add them.
+public class BiggerThanSumOfTwo {
     public static void main(String[] args) {
+        // take two numbers in a row
         int[] a = {11, 2, 5, 7, 9, 15};
         sum(a);
     }
 //find the sum of the first two and then find the number bigger than the sum
 
     private static void sum(int[] a) {
-        for (int i = 1; i < a.length-2; i++) {
+        for (int i = 1; i < a.length; i++) {
             boolean isDone = false;
             int sum = a[i] + a[i - 1];
             int FSum = sum;
             int Fsum = findSmaller(a, i, sum, FSum, isDone);
             if (isDone || Fsum == -1) {
-                System.out.println("No smaller");
+                if (i != a.length - 1) {
+                    System.out.println("No smaller");
+                }
+            }
+            if (i == a.length - 1) {
+                System.out.println(sum);
             }
 
         }
     }
 
     private static int findSmaller(int[] a, int i, int FSum, int sum, boolean isDone) {
-        for (int j = i + 1; j < a.length+1; j++) {
+        for (int j = i + 1; j < a.length; j++) {
             int a1 = a[j];
-            boolean isSmall = isSmall(a1, sum);
+            boolean isSmall = isSmall(a1, sum);// pass by value
             if (isSmall) {
                 FSum = FSum + a1;
                 System.out.println(FSum);
@@ -29,20 +36,19 @@ public class BiggerThanSumOfFirstTwo {
                 return FSum;
 
 
+            } /*else if (i == a.length) {
+                System.out.println(sum);
             }
             if (a.length - 2 == i) {
                 System.out.println(FSum);
                 return FSum;
-            }
+            }*/
         }
         return -1;
     }
 
     private static boolean isSmall(int a1, int sum) {
-        if (a1 < sum) {
-            return true;
-        }
-        return false;
+        return a1 < sum;
     }
 
 
--- src/PatternPractice.java
+++ src/PatternPractice.java
@@ -1,32 +1,40 @@
 public class PatternPractice {
     public static void main(String[] args) {
-        int n = 5;
-        StarRows(n);
+        int n = 3;
+        //StarRows(n);
         System.out.println("-----------------------------------");
         StarTriangle(n);
     }
 
-    private static void StarRows(int n) {
+   /* private static void StarRows(int n) {
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < n - 1; j++) {
                 System.out.print("*");
 
             }
-            System.out.println("*");
+            System.out.println(" ");
 
         }
     }
-
+*/
     private static void StarTriangle(int n) {
+        int k = n + 1;
         //System.out.println("*");
-        for (int i = 0; i < n; i++) {
-            for (int j = 1; j <= i+1; j++) {
-                System.out.print(j);
+        for (int i = -1; i < n; i += 2) {
+            k--;
+            for (int j = 0; j < n - k; j+=2) {
+                System.out.print(' ');
+            }
+
+            for (int j = n; j >= i; j--) {
+                System.out.print("* ");
 
             }
+            for (int j = 0; j < n - k; j+=2) {
+                System.out.print(' ');
+            }
             System.out.println();
-
         }
     }
 }
