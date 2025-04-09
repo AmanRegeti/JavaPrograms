5f51b968321ea051c287d35a7e62851b5692c8d6
AmanRegeti <aman.regeti@gmail.com>
2025-04-08 17:45:09 -0700

    maximum in array in a row

--- src/MaximumSumInArray.java
+++ src/MaximumSumInArray.java
@@ -1,13 +1,27 @@
 public class MaximumSumInArray {
     public static void main(String[] args) {
-        int[] a= {5,1,-3,1,6,9,-1,-9};
-FindMax(a);
+        int[] a = {-5, -1, -3, -1, -6, -4, -1, -2, -9, -1, -9};
+        FindMax(a);
     }
-    private static void FindMax(int[] a){
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
 
+    private static void FindMax(int[] a) {
+        int Max = 0;
+        int k = -1;
+        int l = -1;
+        for (int i = 0; i < a.length; i++) {
+            int e1 = a[i];
+            int cSum = e1;
+            for (int j = i + 1; j < a.length; j++) {
+                int e2 = a[j];
+                cSum += e2;
+                if (cSum > Max) {
+                    Max = cSum;
+                    k = i;
+                    l = j;
+                }
             }
         }
+        System.out.println(Max);
+        System.out.println(k + "-" + l);
     }
 }
