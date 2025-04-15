e82fa81280b8484ea3f8c5ddbb840ae141518423
AmanRegeti <aman.regeti@gmail.com>
2025-04-14 17:15:30 -0700

    Effiecent way of Maximum Sum In Array

--- src/MaximumSumInArray.java
+++ src/MaximumSumInArray.java
@@ -1,27 +1,31 @@
 public class MaximumSumInArray {
     public static void main(String[] args) {
-        //int[] a = {4,-100,4,-100,4,-100,4,-100};
+        //int[] a = {4, -100, 4, -100, 4, -100, 4, -100};
         //int[] a = {-2,1,-3,4,-1,2,1,-5,4};
         //int[] a = {-1,5,4,-1,7,8,2};
         //int[] a = {-2,-2,-1};
-        int[] a = {1};
+        //int[] a = {1,1,4,-1,4,-6,-9,-10};
+        int[] a = { 10, -2,-3};
         FindMax(a);
     }
 
     private static void FindMax(int[] a) {
         int Max = a[0];
-        int k=-1;
-        int l=-1;
+        int k = -1;
+        int l = -1;
         //{ -2,1,-3,4,-1,2,1,-5,4}; 6
+        //{ 10, -2,-3}; 10
         for (int i = 0; i < a.length; i++) {
             int e1 = a[i];
             int cSum = e1;
-            for (int j = i; j < a.length; j++) {
+            if (cSum >= Max) {
+                Max = cSum;
+                k = i;
+                l = i;
+            }
+            for (int j = i + 1; j < a.length; j++) {
                 int e2 = a[j];
                 cSum += e2;
-                if (i == j) {
-                    cSum = cSum / 2;
-                }
                 if (cSum >= Max) {
                     Max = cSum;
                     k = i;
