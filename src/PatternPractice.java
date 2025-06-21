7a3f249d94f18275f933296e2d38e13b3f515d8a
AmanRegeti <aman.regeti@gmail.com>
2025-06-20 19:14:31 -0700

    Exceptions and Errors

--- src/PatternPractice.java
+++ src/PatternPractice.java
@@ -1,41 +1,44 @@
 public class PatternPractice {
     public static void main(String[] args) {
-        int n = 3;
-        //StarRows(n);
+        int n = 6;
+        StarRows(n);
         System.out.println("-----------------------------------");
-        //StarTriangle(n);
+        StarTriangle(n);
+        System.out.println("----------------------");
         NumberTriangle(n);
+        System.out.println("----------------------");
         LetterTriangle(n);
+        System.out.println("----------------------");
         LetterTriangle2(n);
+        System.out.println("----------------------");
+        UpsideDownLetterTriangle(n);
     }
 
-    /* private static void StarRows(int n) {
+    private static void StarRows(int n) {
 
-         for (int i = 0; i < n; i++) {
-             for (int j = 0; j < n - 1; j++) {
-                 System.out.print("*");
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1; j++) {
+                System.out.print("*");
 
-             }
-             System.out.println(" ");
-
-         }
-     }
- */
-    private static void StarTriangle(int n) {
-        ;
-        int o = n;
-        for (int i = 0; i < n; i++, o--) {
+            }
             System.out.println(" ");
-            int k = 0;
-            int l = 0;
-            for (int j = 0; l < n - o; l++) {
+
+        }
+    }
+
+    private static void StarTriangle(int n) {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < i; j++) {
                 System.out.print(" ");
             }
-            for (int j = 0; k < n - i; k++) {
-                System.out.print("* ");
+            for (int j = 0; j < 2 * n - (2 * i + 1); j++) {
+                System.out.print("*");
             }
+            for (int j = 0; j < i; j++) {
+                System.out.println(" ");
+            }
+            System.out.println();
         }
-
     }
 
     private static void ZeroOneTriangle(int n) {
@@ -58,9 +61,10 @@ public class PatternPractice {
             System.out.println();
         }
     }
+
     private static void LetterTriangle(int n) {
 
-        char[] Alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        char[] Alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(Alphabet[j]);
@@ -68,9 +72,10 @@ public class PatternPractice {
             System.out.println();
         }
     }
+
     private static void LetterTriangle2(int n) {
 
-        char[] Alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        char[] Alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 System.out.print(Alphabet[i]);
@@ -78,5 +83,18 @@ public class PatternPractice {
             System.out.println();
         }
     }
+    private static void UpsideDownLetterTriangle(int n) {
+        char[] Alphabet = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+        for (int i = 0; i < n; i++)
+        {
+        int k=i;
+            for (int j=0;0 <n-k; j++,k++)
+            {
+                System.out.print(Alphabet[j]);
+            }
+            System.out.println();
 
-}
+            }
+        }
+
+    }
---
+++ src/exceptions/Explanation.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class Explanation {
+    /*
+               throwable
+    |                           |
+    EXCEPTION       and        Error
+    |                            |
+ checked and unchecked        StackOverFlowError
+    |           |
+    IO,SQL
+      */
+}
