533bd3ef399b31e2140f612ef774083893c77a3d
AmanRegeti <aman.regeti@gmail.com>
2025-08-16 11:41:31 -0700

    Sorting 3 array into 1

--- src/SortedArraysInSortedArray.java
+++ src/SortedArraysInSortedArray.java
@@ -6,40 +6,43 @@ public class SortedArraysInSortedArray {
         int[] a = {3, 7, 8, 10, 14, 20};
         int[] b = {4, 6, 12, 15, 17, 19};
         int[] c = {1, 2, 5, 9, 11, 13, 16, 18};
-
+        int[] d = {21};
         int[] MergedArray = sortArray(a, b);
-        System.out.println(Arrays.toString(MergedArray));
-        System.out.println("hi");
+        int[] FinishedArray= sortArray(MergedArray,c);
+        int[] FinishedArray2= sortArray(FinishedArray,d);
+        System.out.println(Arrays.toString(FinishedArray2));
+
+
     }
+
 
     private static int[] sortArray(int[] a, int[] b) {
         int[] MergedArray = new int[a.length + b.length];
         int count = 0;
-        int length = b.length;
         int i = 0;
         int j=0;
-        if (a.length >= b.length) {
-            length = a.length;
-
-        }
-        for (i < length && j < length; ) {
+        for (;i < a.length && j < b.length;count++) {
             int e1 = a[i];
             int e2 = b[j];
             if (e1 <= e2) {
                 MergedArray[count] = e1;
                 i++;
-                count++;
+
             } else {
                 MergedArray[count] = e2;
                 j++;
-                count++;
+
             }
 
         }
-        if (i<j){
+        for (; j < b.length; j++) {
+            MergedArray[count] = b[j];
+            count++;
+        }
+        for (; i < a.length; i++) {
             MergedArray[count] = a[i];
-        }else{
-
+            count++;
+        }
         return MergedArray;
     }
 }
