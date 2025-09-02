c686f294a4fd993c8bea1de949cae1e0f60812cc
AmanRegeti <aman.regeti@gmail.com>
2025-09-01 19:06:05 -0700

    Finding 6 in an array

--- src/Find6.java
+++ src/Find6.java
@@ -1,35 +1,54 @@
+import java.util.Arrays;
+
 public class Find6 {
     public static void main(String[] args) {
-        int[] a = {1, 5, 7, 8, 2, 3, 5,6};
-        try {
-           int sum= Find6(a);
-            System.out.println(sum);
-        } catch (Exception e) {
-            System.out.println("There was no 6 in this Array or was at the starting or end");
-        }
+        int[] a = { 7, 2,6, 3,};
+        int sum = 0;
+//        try {
+//            Finding6(a);
+//        } catch (No6Exception e) {
+//            System.out.println("There was no 6 in this Array");
+//            return;
+//        } catch (StartingOrEnding6 f) {
+//
+//        }
+
+            int[] answer =Finding6(a);
+
+        System.out.println(Arrays.toString(answer));
+
     }
 
-    private static int Find6(int[] a) throws No6Exception,StartingOrEnding6{
+
+    private static int[] Finding6(int[] a){
         for (int i = 0; i < a.length; i++) {
-            if (a[0] == 6 || a[a.length - 1] == 6) {
-                try{
-                    throw new StartingOrEnding6("There was no 6 in this Array");
-                }catch (StartingOrEnding6 f){
-                    throw f;
-                }
-            }else if (a[i] == 6) {
-                int sum = equals(a, i);
-                return sum;try {
-                    throw new No6Exception("There was no 6 in this Array");
-                }catch (No6Exception e){
-                    throw e;
-                }
+        if(a[i]==6){
+            if (i==a.length-1||i==0){
+                int[] remaining = new int[a.length-1];
+                for (int j = 0,k=0; j < a.length; j++ ) {
+                    if (a[j]!=6){
+                        remaining[k] = a[j];
+                        k++;
+                    }
             }
+                return remaining;
+            }
+            else {
+                int sum =equals(a,i);
+                int[] answer={sum};
+                return answer;
+            }
+
+            }
+
+        }
+        return a;
     }
 
-    private static int equals(int[] a,int i) {
-        int sum=0;
-        sum=a[i+1]+a[0];
+    private static int equals(int[] a, int i) {
+        int sum = 0;
+        sum = a[i + 1] + a[0];
         return sum;
     }
 }
+
