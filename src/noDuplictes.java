d09b557890c1a05c9a2fa7cf952083e2803364f8
AmanRegeti <aman.regeti@gmail.com>
2025-10-22 16:54:52 -0700

    remove duplicates no hashset

--- src/noDuplictes.java
+++ src/noDuplictes.java
@@ -1,30 +1,36 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class noDuplictes {
     public static void main(String[] args) {
-        int[] a = {2, 4, 7, 1, 3, 4, 6, 7};
+        int[] a = {2, 4, 7,1,1,1,1,1,1,1,1,1,1, 1, 3, 4, 6, 7, 7,2, 4, 7,1,1,1,1,1,1,1,1,1,1, 1, 3, 4, 6, 7, 7,2, 4, 7,1,1,1,1,1,1,1,1,1,1, 1, 3, 4, 6, 7, 7};
 
         findingIfAndRemovingDuplicates(a);
     }
 
     private static void findingIfAndRemovingDuplicates(int[] a) {
-        int[] b = new int[a.length];
-        for (int i = 0; i < a.length; i++) b[i] = a[i];;
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length; j++) {
-                int e1 = a[i];
-                int e2 = a[j];
-                if(i != j) {
-                    if (e1 == e2) {
-                        b[i] = a[i];
-                        b[j] = 0;
+            for (int i = 0; i < a.length; i++) {
+                for (int j = 0; j < a.length; j++) {
+                    int e1 = a[i];
+                    int e2 = a[j];
+                    if (i != j) {
+                        if (e1 == e2) {
+                            a[j]=0;
+                        }
                     }
                 }
-            }
         }
-        System.out.println(Arrays.toString(b));
+        removeZero(a);
     }
-    private static void moveZeroRight(int[] b){
+    private static void removeZero (int[] a){
+        ArrayList b = new ArrayList();
+        for (int i = 0; i < a.length; i++) {
+           int e1= a[i];
+           if (e1!=0){
+                b.add(e1);
+           }
+        }
+        System.out.println(Collections.unmodifiableList(b));
     }
 }
