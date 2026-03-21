7632710f1a8129db2f0f322628e554d57ab27aa4
AmanRegeti <aman.regeti@gmail.com>
2026-03-20 17:44:42 -0700

    Threads

---
+++ src/TicketBooking.java
@@ -0,0 +1,24 @@
+class TicketBooking implements Runnable {
+    @Override
+    public void run() {
+
+        try {
+
+            // Timed waiting
+            Thread.sleep(200);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("State of bookingThread while mainThread is waiting: " +
+                TicketSystem.ticketSytesmThread.getState());
+
+        try {
+
+            // Another timed waiting
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/TicketSystem.java
@@ -0,0 +1,45 @@
+public class TicketSystem implements Runnable {
+        public static Thread ticketSytesmThread;
+        public static TicketSystem ticketSystem;
+
+        @Override
+        public void run() {
+            TicketBooking booking = new TicketBooking();
+            Thread bookingThread = new Thread(booking);
+
+            System.out.println("State after creating bookingThread: " + bookingThread.getState());
+
+            bookingThread.start();
+            System.out.println("State after starting bookingThread: " + bookingThread.getState());
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("State after sleeping bookingThread: " + bookingThread.getState());
+
+            try {
+
+                // Moves mainThread to waiting state
+                bookingThread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println("State after bookingThread finishes: " + bookingThread.getState());
+        }
+
+        public static void main(String[] args) {
+            ticketSystem = new TicketSystem();
+            ticketSytesmThread = new Thread(ticketSystem);
+
+            System.out.println("State after creating ticketSytesmThread: " + ticketSytesmThread.getState());
+
+            ticketSytesmThread.start();
+            System.out.println("State after starting mainThread: " + ticketSytesmThread.getState());
+
+            System.out.println("Main Thread is finished");
+        }
+    }
